faf264a45c271ab53c7d30b699a9ed6f08239600
rukonit <rukon-it@live.com>
2021-07-03 22:03:38 -0400

    login system development

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,6 +1,7 @@
 import { Component } from "@angular/core";
 import { Form, NgForm } from "@angular/forms";
-import { AuthService } from "./auth.service";
+import { Observable } from "rxjs";
+import { AuthResponseData, AuthService } from "./auth.service";
 
 
 @Component({
@@ -14,6 +15,8 @@ export class AuthComponent {
     isLoginMode = true;
     isLoading = false;
     error: string = '';
+
+   
 
     constructor(private authService: AuthService) {
 
@@ -29,24 +32,26 @@ export class AuthComponent {
         }
         const email= form.value.email;
         const password = form.value.password;
+        let authObs: Observable<AuthResponseData>;
 
         this.isLoading = true;
-        if (this.isLoginMode) {}
+        if (this.isLoginMode) {
+           authObs = this.authService.login(email, password)
+        }
 
         else {
         
-            this.authService.signUp(email, password).subscribe((resData) => {console.log(resData);
-            setTimeout(()=>{ this.isLoading= false;
-                
-            }, 2000)
-           
-            
-          
-            }, errorMessage => {
+           authObs = this.authService.signUp(email, password)
+        }
+
+        authObs.subscribe((resData) => {
+            console.log(resData);
+            this.isLoading= false;      
+            }, 
+            errorMessage => {
                 this.error = errorMessage
                 this.isLoading = false;
             })
-        }
        
         form.reset();
         
--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,39 +1,69 @@
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpErrorResponse } from "@angular/common/http";
 import { Injectable } from "@angular/core";
-import { catchError } from "rxjs/operators";
-import { throwError } from "rxjs";
-import { error } from "protractor";
+import { catchError, tap } from "rxjs/operators";
+import { Subject, throwError } from "rxjs";
+import { User } from "./user.model";
 
-interface AuthResponseData {
+
+export interface AuthResponseData {
     kind: string,
-    idToken: string
+    idToken: string,
     email: string,
     refreshToken: string,
-    expiresIn: string
-    localId: string
+    expiresIn: string,
+    localId: string,
+    registered?: boolean;
     
+
 }
 
 @Injectable({providedIn: 'root'})
 export class AuthService {
-
+    user = new Subject<User>()
     constructor(private http: HttpClient) {}
     signUp(email: string, password: string) {
         return this.http.post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCgn1gbgJqHlg8ekd0zjFWhK2ZpjGp_ss8', 
         {email: email, password: password, returnSecureToken: true}, {headers: {'content-type' : 'application/json'}})
-        .pipe(catchError(errorRes => {
-            let errorMessage = 'An unknown error occured'
-            if (!errorRes.error || !errorRes.error.error) {
-                return throwError(errorMessage)
-            }
-            
-            switch (errorRes.error.error.message) {
-                case 'EMAIL_EXISTS':
-                errorMessage = 'This email already exists'
-            }
-            return throwError(errorMessage);
-        } ) );
+        .pipe(catchError(this.handleError), tap(resData=>{
+            this.handleAuthentication(resData.email, resData.localId, resData.idToken, +resData.expiresIn)
+        }));
         
 
+    }
+
+    login(email: string, password: string) {
+        return this.http.post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyCgn1gbgJqHlg8ekd0zjFWhK2ZpjGp_ss8', 
+        {email: email, password: password, returnSecureToken: true})
+        .pipe(catchError(this.handleError), tap(resData=>{
+            this.handleAuthentication(resData.email, resData.localId, resData.idToken, +resData.expiresIn)
+        }));
+    }
+
+    private handleError(errorRes: HttpErrorResponse) {
+
+        let errorMessage = 'An unknown error occured'
+        if (!errorRes.error || !errorRes.error.error) {
+            return throwError(errorMessage)
+        }
+        
+        switch (errorRes.error.error.message) {
+            case 'EMAIL_EXISTS':
+            errorMessage = 'This email already exists'
+            break;
+            case 'EMAIL_NOT_FOUND':
+            errorMessage = 'There is no user record corresponding to this identifier. The user may have been deleted'
+            break;
+            case 'INVALID_PASSWORD':
+            errorMessage = 'The password is invalid or the user does not have a password.'
+        }
+        return throwError(errorMessage);
+
+    }
+
+    handleAuthentication(email: string, userId: string, token: string, expiresIn: number) {
+            
+        const expirationDate = new Date(new Date().getTime() + expiresIn * 1000);
+        const user = new User(email, userId, token, expirationDate);
+        this.user.next(user);
     }
 }
