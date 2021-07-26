3e670caea0f5e42f8de6834c2b14e018766823c8
rukonit <rukon-it@live.com>
2021-07-26 12:53:56 -0400

    updated menu css transition

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -11,7 +11,8 @@ import * as fromRecipeActions from '../recipes/store/recipes.actions';
 @Component(
     {
         selector: 'app-header',
-        templateUrl: './header.component.html'
+        templateUrl: './header.component.html',
+        styleUrls: ['./header.component.css']
     }
 )
 export class HeaderComponent implements OnInit, OnDestroy {
